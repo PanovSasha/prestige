de22851d24cb81fc48b9be4d914fd6605badb4cd
Sasha <aleksander.panov@synchro.pro>
2023-01-29 14:05:48 +0300

    вырезка лишнего

--- src/js/common/index.js
+++ src/js/common/index.js
@@ -1,5 +1,2 @@
 export { changeToplineMod, mobileMenu } from './topline';
-export { createCustomCursors } from './custom-cursor';
-export { initTabs } from './tabs';
 export { overlays } from './overlay';
-export { goBackButton } from './go-back';
--- src/js/index.js
+++ src/js/index.js
@@ -7,57 +7,21 @@ import './lib/icons';
 // Plugins
 
 
-import { changeToplineMod, createCustomCursors, goBackButton, initTabs, mobileMenu, overlays } from './common';
+import { changeToplineMod, mobileMenu, overlays } from './common';
 
-import {
-  addAssetsLogos,
-  contactFormValidate,
-  datePicker,
-  search,
-  withTabs
-} from './blocks';
 
-import { selects, period } from './ui';
 
-import {
-    materials,
-    materialsPass,
-    ourValuesSlider,
-    partnersSlider,
-    profitsSliders,
-    withUsSlider,
-} from './pages';
 
 // Common
 changeToplineMod();
 mobileMenu();
-createCustomCursors();
-initTabs();
 overlays();
-goBackButton();
 
 // UI
-selects();
-period();
 
 // Blocks
-addAssetsLogos();
-search();
-withTabs();
 
 // Home
-profitsSliders();
-partnersSlider();
-
-// Press-center-pages
-datePicker();
-materials();
-materialsPass();
-
-// Career
-ourValuesSlider();
-withUsSlider();
 
 // Contacts
-contactFormValidate();
 
